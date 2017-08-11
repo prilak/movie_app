577be8dfc4831d52a807045d80b212914bad771f
Michael <mdlawrence@utexas.edu>
2017-08-10 19:22:14 -0500

    added request api

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+
+app.get("/results", function(req, res){
+	request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb", function(error, response, body){
+		if(!error && response.statusCode == 200){
+			var results = JSON.parse(body);
+			res.send(results["Search"][0]);
+		}
+	});
+});
+
+app.listen(3000);
